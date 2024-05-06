181388cfc28f08c1717d5a8ec0aac17b794676d3
weirdball7 <moteamitay04@gmail.com>
2024-05-06 19:57:20 +0300

    still not sure what im doing but ill make it work you can now put 1 x on 1 square with a click of the mouse

--- main.js
+++ main.js
@@ -7,10 +7,18 @@ const Gameboard = (() => {
             boardHTML += `<div class="square" id=square-${index}">${square}</div>`;
         });
         document.querySelector("#gameboard").innerHTML = boardHTML;
+
+        
     };
+
+    const update = (index, value) => {
+        gameboard[index] = value;
+        render();
+    }
 
     return {
         render,
+        update, 
     }
 })();
 
@@ -23,19 +31,36 @@ const createPlayer = (name, mark) => {
 
 const Game = (() => {
     let players = [];
-    let currentPlayerIndex = 0;
-    let gameOver = false;
+    let currentPlayerIndex;
+    let gameOver;
 
     const start = () => {
         players = [
             createPlayer(document.querySelector("#player1").value, "X"),
             createPlayer(document.querySelector("#player2").value, "O"),
         ];
+        currentPlayerIndex = 0;
+        gameOver = false;
+        players.forEach((player) => {
+            console.log(player);
+        })
         Gameboard.render();
+        let squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.addEventListener('click', handleClick)
+        });
+    };
+
+    
+
+    const handleClick = (event) => {
+        let index = parseInt(event.target.id.split("-")[1]);
+        Gameboard.update(index, players[currentPlayerIndex].mark);        
     };
 
     return {
         start,
+        handleClick,
     };
 })();
 
