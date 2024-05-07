4a808bd17a3f5e082c19cad7bb448091b61d0a3f
weirdball7 <moteamitay04@gmail.com>
2024-05-07 22:26:38 +0300

    i know a litle bit what im doing now; ive started working on implimenting the main gaim logic: checking for win conditions every round

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
+// This is the Gameboard module
+//it is in charge of rendering and updating the board.
 const Gameboard = (() => {
-    let gameboard = ["", "", "", "", "", "", "", "", "", ];
+    let gameboard = ["", "", "", "", "", "", "", "", "", ]; 
+    // ocupiedSpaces = [];
+
 
     const render = () => {
-        let boardHTML = "";
+        console.log(gameboard);
+        let boardHTML = ""; // board div; this is where the squares go
         gameboard.forEach((square, index) => {
             boardHTML += `<div class="square" id=square-${index}">${square}</div>`;
         });
         document.querySelector("#gameboard").innerHTML = boardHTML;
-
-        
+        let squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.addEventListener('click', Game.handleClick);
+        });
     };
 
     const update = (index, value) => {
+        // let squares = document.querySelectorAll(".square");
         gameboard[index] = value;
-        render();
+        render(); 
+        // squares.forEach((square) => {
+        //     square.addEventListener('click', Game.handleClick)
+        // });
     }
 
     return {
@@ -30,9 +41,12 @@ const createPlayer = (name, mark) => {
 };
 
 const Game = (() => {
+    let ocupiedSpaces = [];
     let players = [];
     let currentPlayerIndex;
     let gameOver;
+
+
 
     const start = () => {
         players = [
@@ -41,21 +55,27 @@ const Game = (() => {
         ];
         currentPlayerIndex = 0;
         gameOver = false;
-        players.forEach((player) => {
-            console.log(player);
-        })
+        // players.forEach((player) => {
+        //     console.log(player);
+        // })
         Gameboard.render();
-        let squares = document.querySelectorAll(".square");
-        squares.forEach((square) => {
-            square.addEventListener('click', handleClick)
-        });
     };
 
-    
+    const playRound = () => {
+
+    };
 
     const handleClick = (event) => {
+        
         let index = parseInt(event.target.id.split("-")[1]);
-        Gameboard.update(index, players[currentPlayerIndex].mark);        
+        ocupiedSpaces.unshift(index);
+        console.log(ocupiedSpaces);
+            
+
+
+        Gameboard.update(index, players[currentPlayerIndex].mark);
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+        return ocupiedSpaces;
     };
 
     return {
